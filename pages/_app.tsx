328d351d50374120688fdea1a7a5aada77bba00a
lmazouin <53606603+LMazouin@users.noreply.github.com>
2022-05-16 14:33:49 +0200

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+//import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Projet WebForce3</title>
+        <meta name="description" content="webforce3" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default App;
---
+++ server.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import next from "next";
+import express from "express";
+import { IncomingMessage, ServerResponse } from "http";
+
+const dev = process.env.NODE_ENV !== "production";
+
+const hostname = "localhost";
+const port = 3000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  try {
+    const server = express();
+
+    server.get("*", (req: IncomingMessage, res: ServerResponse) => {
+      return handle(req, res);
+    });
+
+    server
+      .listen(port, hostname, () => {
+        console.log("> Ready on http://localhost:3000");
+      })
+      .once("error", (error: Error) => {
+        throw error;
+      });
+  } catch (error) {
+    if (error instanceof Error)
+      console.error("internal server error", error.stack);
+    process.exit(1);
+  }
+});
