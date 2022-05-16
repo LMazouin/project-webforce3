2e0456cfd57c328288f00e64b297598becfae2ab
lmazouin <53606603+LMazouin@users.noreply.github.com>
2022-05-16 16:12:07 +0200

    connect to database

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
+import { IncomingMessage, ServerResponse } from "http";
 import "dotenv/config";
 import next from "next";
 import express, { Request, Response } from "express";
-import { IncomingMessage, ServerResponse } from "http";
+import mongoose, { Connection } from "mongoose";
+import * as logger from "morgan";
 
 const dev = process.env.NODE_ENV !== "production";
 
@@ -11,9 +13,27 @@ const port = 3000;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
+function connectToMongoDB(uri: string): Connection {
+  mongoose.connect(uri);
+  return mongoose.connection
+    .on("connected", console.info.bind(logger, "CONNECTED TO MONGODB"))
+    .on("disconnected", console.info.bind(logger, "DISCONNECTED FROM MONGODB"))
+    .on("reconnected", console.info.bind(logger, "RECONNECTED TO MONGODB"))
+    .on("error", console.info.bind(logger, "MONGODB ERROR"));
+}
+
 app.prepare().then(() => {
   try {
     const server = express();
+
+    process.on("SIGINT", () => {
+      console.log("\rCLOSING CONNECTION TO MONGODB");
+      mongoose.connection.close();
+      console.log("SHUTTING DOWN SERVER");
+      process.exit(0);
+    });
+
+    if (process.env.MONGODB_URI) connectToMongoDB(process.env.MONGODB_URI);
 
     server.get("*", (req: IncomingMessage, res: ServerResponse) => {
       return handle(req, res);
