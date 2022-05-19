3f0cb966941c3552d582e9a259392b7aca084b9d
lmazouin <53606603+LMazouin@users.noreply.github.com>
2022-05-19 11:29:54 +0200

    add settings.tsx

---
+++ pages/settings.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Typography from "@mui/material/Typography";
+import Menu from "../components/lib/Menu";
+
+const Settings: NextPage = (): JSX.Element => (
+  <Menu>
+    <Typography paragraph>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio repellendus autem praesentium enim fuga maxime
+      suscipit! Voluptatum repellendus eum dolorum eaque ex consequatur dicta? Dolore excepturi nisi aspernatur quo
+      quisquam!
+    </Typography>
+  </Menu>
+);
+
+export default Settings;
