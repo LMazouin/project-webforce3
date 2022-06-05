d156ef9c1d49960b0142e43066a8c6932c22f0cb
lmazouin <53606603+LMazouin@users.noreply.github.com>
2022-06-05 15:48:48 +0200

    fix eslint errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,10 +18,7 @@ export default function App(props: AppProps<PageProps>): JSX.Element {
       <Head>
         <title>Projet WebForce3</title>
         <meta name="description" content="webforce3" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <CssBaseline />
